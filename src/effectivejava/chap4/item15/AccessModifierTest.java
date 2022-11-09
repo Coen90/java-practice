d42abdc03d9b27827ccd012221280c0cc38c66b5
hyuntae <bht9011@gmail.com>
2022-11-10 07:42:35 +0900

    study before work 20221109

--- src/effectivejava/chap4/item15/AccessModifierTest.java
+++ src/effectivejava/chap4/item15/AccessModifierTest.java
@@ -6,6 +6,6 @@ public class AccessModifierTest {
     String packagePrivateName = "ヒョンテ"; // 선언된 클래스가 포함된 패키지에서 접근 가능
     protected String protectedName = "炫炱"; // package private의 범위를 포함하여 상속받은 하위클래스에서 접근 가능
     public String publicName = "Hyuntae"; // 다른 패키지에서 접근 가능
-    public static final String[] VALUE = {"안녕", "하세요"};
+    public static final String[] VALUE = {"안녕", "하십니까"};
 
 }
