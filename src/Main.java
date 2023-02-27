74d32bf7ed62e26946162f62d0dda16db8139484
hyuntae <bht9011@gmail.com>
2023-02-27 20:48:52 +0900

    equals and hashcode override test

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,52 @@
-class Atom {
-    Atom() {
-        System.out.print("atom ");
-    }
-}
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
-class Rock extends Atom {
-    Rock(String type) {
-        System.out.print(type);
-    }
-}
+public class Main {
+    public static class Equals {
+        private int number;
+        private String name;
 
-public class Main extends Rock {
-    Main() {
-        super("granite ");
-        new Rock("granite ");
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Equals equals = (Equals) o;
+
+            if (number != equals.number) return false;
+            return Objects.equals(name, equals.name);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = number;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+
+        public Equals(int number, String name) {
+            this.number = number;
+            this.name = name;
+        }
     }
 
-    public void getMain() {
-        System.out.println(":asd".concat("123"));
-    }
+    public static void main(String[] args) {
+        Equals e1 = new Equals(1, "hyuntae");
+        Equals e2 = new Equals(1, "hyuntae");
 
-    public static void main(String[] a) {
-        new Main().getMain();
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e1 == e2);
+        System.out.println(e1.equals(e2));
+
+        Set<Equals> set = new HashSet<>();
+
+        set.add(e1);
+        set.add(e2);
+
+        System.out.print("Set.size = ");
+        System.out.println(set.size());
+
     }
 }
