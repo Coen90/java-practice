6f1e173fc22283c1c8b97aa637d61c7103050381
hyuntae <bht9011@gmail.com>
2024-07-12 22:20:52 +0900

    Baekjoon 20437 solution

--- src/coding/test/baekjoon/Bj20437.java
+++ src/coding/test/baekjoon/Bj20437.java
@@ -5,62 +5,64 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bj20437 {
+    static int T;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int game = Integer.parseInt(br.readLine());
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < game; i++) {
-            String str = br.readLine();
-            int k = Integer.parseInt(br.readLine());
-            result.append(solution(str, k));
+
+        T = Integer.parseInt(br.readLine());
+        while(T --> 0) {
+            String W = br.readLine();
+            int K = Integer.parseInt(br.readLine());
+
+            solve(W, K);
         }
-        System.out.println(result);
+
+        System.out.println(sb.toString());
     }
 
-    public static String solution(String str, int k) {
-        StringBuilder sb = new StringBuilder();
-        //map을 사용해서 key:알파벳 value:[]idx
-        //알파벳 갯수 세기
-        int[] alphabet = new int[26];
-        for (int i = 0; i < str.length(); i++) {
-            int idx = str.charAt(i) - 'a';
-            alphabet[idx]++;
+    private static void solve(String w, int k) {
+        if(k == 1) {
+            sb.append("1 1").append("\n");
+            return;
         }
 
-        //Min Max 구하기
-        int min = Integer.MAX_VALUE;
-        int max = -1;
-        for (int i = 0; i < str.length(); i++) {
-            int idx = str.charAt(i) - 'a';
-            //특정 알파벳이 k개에 못미치면 패스
-            if (alphabet[idx] < k) {
+        // 알파벳 별로 개수 저장하기
+        int[] alpha = new int[26];
+        for(int i = 0 ; i < w.length() ; i++) {
+            alpha[w.charAt(i) - 'a']++;
+        }
+
+        int min = Integer.MAX_VALUE; // 최소 길이
+        int max = -1; // 첫 번째와 마지막 글자가 같은 가장 긴 연속 문자열의 길이
+
+        for(int i = 0 ; i < w.length() ; i++) {
+            // 해당 문자의 갯수가 k개 이하라면 탐색 X
+            if(alpha[w.charAt(i) - 'a'] < k)
                 continue;
-            }
 
-            if (k == 1) {
-                min = 1;
-                max = 1;
-            }
+            int cnt = 1;
 
-            int count = 1;
-            for (int j = i+1; j < str.length(); j++) {
-                if (str.charAt(j) == str.charAt(i)) {
-                    count++;
-                }
-                if (count == k) {
-                    min = Math.min(min, j - i + 1);
-                    max = Math.max(max, j - i + 1);
+            // 뒷 문자열과 비교
+            for(int j = i+1 ; j < w.length() ; j++) {
+                if(w.charAt(i) == w.charAt(j))
+                    cnt++;
+
+                // 같은 문자의 수가 k개가 되는 순간
+                if(cnt == k) {
+                    min = Math.min(min, j-i+1);
+                    max = Math.max(max, j-i+1);
                     break;
                 }
             }
         }
 
-        if (min == Integer.MAX_VALUE || max == -1) {
-            sb.append(-1);
-        } else {
-            sb.append(min).append(" ").append(max);
+        if(min == Integer.MAX_VALUE || max == -1) {
+            sb.append("-1").append("\n");
+            return;
         }
-        sb.append("\n");
-        return sb.toString();
+
+        sb.append(min + " " + max).append("\n");
     }
 }
